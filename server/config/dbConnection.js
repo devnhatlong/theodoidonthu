9940901f2f07fa7c1c632421fb586be8773d4573
Raito Nguyen <raito.n@itcgroup.io>
2024-07-08 10:44:43 +0700

    handle authentication mongodb

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -1,20 +1,23 @@
-const { default: mongoose} = require("mongoose")
+const { default: mongoose } = require("mongoose");
 
-const dbConnect = async () => { 
+const dbConnect = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
-        
+        const uri = `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@localhost:27017/theodoidonthu?authSource=admin`;
+        const conn = await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
         if (conn.connection.readyState === 1) {
             console.log("DB connecting is successfully");
-        }
-        else {
+        } else {
             console.log("DB connecting");
         }
-        
+
     } catch (error) {
         console.log("DB connection is failed");
         throw new Error(error);
     }
 }
 
-module.exports = dbConnect
+module.exports = dbConnect;
